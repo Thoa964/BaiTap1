53db1edef7b80795ec1f3d1371201eab27941fcf
Thoa964 <84794644+Thoa964@users.noreply.github.com>
2022-09-24 17:06:41 +0700

    Tuan 3

---
+++ BaiTap_NguyenThiThoa/BaiTap3_NguyenThiThoa/man.rb
@@ -0,0 +1,18 @@
+class Man
+    attr_accessor :name, :hair ,:gender , :phone, :email, :nation
+    attr_accessor :height, :weight, :age
+    def initialize(name,gender,hair,height,weight,age,phone,email,nation)          
+            @name = name    
+            @gender = gender    
+            @hair = hair
+            @height = height
+            @weight = weight
+            @phone =  phone
+            @email = email 
+            @age = age
+            @nation = nation 
+    end 
+    def show
+        puts "Tên : #{name}, Giới tính: #{gender},  Tuổi: #{age}, Tóc: #{lan}, Chiều cao: #{height}, Cân nặng: #{height}, Số điện thoại: #{phone}, Email: #{email}, Dân tộc: #{nation}\n"
+    end 
+end 
---
+++ BaiTap_NguyenThiThoa/BaiTap3_NguyenThiThoa/student.rb
@@ -0,0 +1,34 @@
+require_relative 'man.rb'
+
+
+
+class Student < Man
+    attr_accessor :grade1, :grade2, :grade3, :average 
+    def nhap(grade1,grade2,grade3)
+        @grade1 = grade1
+        @grade2 = grade2
+        @grade3 = grade3
+        @average = (grade1 + grade2 + grade3)*0.1 /3
+    end  
+    def show
+        puts "Tên : #{name}\n  Giới tính: #{gender}\n  Tuổi: #{age}\n  Tóc: #{hair}\n  Chiều cao: #{height}\n  Cân nặng: #{height}\n  Số điện thoại: #{phone}\n  Email: #{email}\n  Dân tộc: #{nation}\n  Điểm trung bình: #{average}\n"
+    end
+end
+a = Array.new()
+Student1 = Student.new("Thoa","Nu","Đen","1m58","47kg","21","0397927870","thoa2503@gmail.com","Kinh")
+Student1.nhap(9,9,10)
+a.push(Student1)
+Student2 = Student.new("Nhi","Nữ","Đen","1m6","45kg","19","0253145875","nhi@gmail.com","Kinh")
+Student2.nhap(7,8,9)
+a.push(Student2)
+Student3= Student.new("Ngan","Nữ","Đen","1m7","45kg","22","02397852321","ngan@gmail.com","Kinh")
+Student3.nhap(7,10,9)
+a.push(Student3)
+a.each_index {
+    |index|
+    puts "#{a[index].show()}"
+}
+print "Danh sách sinh viên sau khi sắp xếp theo thứ tự giảm dần:\n"
+for x in a.sort_by{|stu|[-stu.average]} do
+    puts "#{x.show}"
+end
